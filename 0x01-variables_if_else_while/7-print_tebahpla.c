e9808930ebb086fd00756b43b40b33bb6d4a06e4
AxiomDays <tobeoke@gmail.com>
2023-06-19 18:21:58 -0700

    taskclean

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char i;
+
 	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
