b8d4849c7eb682115cd90fa426490cc0c58b0209
AxiomDays <tobeoke@gmail.com>
2023-06-19 18:01:11 -0700

    taskclean

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* yadda yadda yadda */
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
