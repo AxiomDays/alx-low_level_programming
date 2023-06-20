b1773c0b750511f115f7d50a4439a2d1317c36fe
AxiomDays <tobeoke@gmail.com>
2023-06-19 17:44:37 -0700

    task8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,8 +7,12 @@ int main(void)
 	for (x = 0; x <= 9; x++)
 	{
 		putchar(48 + x);
-		putchar(',');
-		putchar(' ');
+
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
