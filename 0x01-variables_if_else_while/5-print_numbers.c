14989682e3ce2622c7dad221b740fbfd23e67446
AxiomDays <tobeoke@gmail.com>
2023-06-19 18:19:03 -0700

    taskclean

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		printf("%d",x);
+		printf("%d", x);
 	}
 	putchar('\n');
 	return (0);
