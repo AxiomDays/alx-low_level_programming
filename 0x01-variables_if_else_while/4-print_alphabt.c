156f10708aee60081734ffc84af2e737825cd7e1
AxiomDays <tobeoke@gmail.com>
2023-06-19 18:17:20 -0700

    taskclean

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int x;
 
-	for (x = 'a';x <= 'z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		if (x == 'q' || x == 'e')
 		{
