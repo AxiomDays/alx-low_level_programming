f5a4d50d23c74b7c353d44d66be35265ccb6bee0
Axiom Days <tobeoke@gmail.com>
2023-06-21 13:56:13 +0100

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include"main.h"
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	int i;
