ca14944a170847dfda10e21251c1ca4458b46b70
Axiom Days <tobeoke@gmail.com>
2023-06-21 23:04:38 +0100

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @c: int c
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+        if (c > 64 && c < 123)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
+
