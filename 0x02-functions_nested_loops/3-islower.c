b3175c50b869792d8f7a9de1b5b2ca214c1d1766
Axiom Days <tobeoke@gmail.com>
2023-06-21 22:51:33 +0100

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include"main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @parameter: int c
+ * Return: Always 0 (Success)
+ */
+
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
