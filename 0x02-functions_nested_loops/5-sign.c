26ef6bac73c6679e147865490a5546e7fd099f97
Axiom Days <tobeoke@gmail.com>
2023-06-21 23:42:13 +0100

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,7 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		putchar('+');
+		putchar(43);
 	}
 	else
 	{
@@ -17,7 +17,7 @@ int print_sign(int n)
 		else
 		{
 			return (-1);
-			putchar('-');
+			putchar(45);
 		}
 	}
 }
