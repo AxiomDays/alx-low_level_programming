5a1ba738c05a19d749e20e7444d90dbbf7cf19e6
Axiom Days <tobeoke@gmail.com>
2023-06-22 00:55:43 +0100

    task6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include"main.h"
-
+/**
+ * _abs - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @n : int n
+ * Return: Always 0 (Success)
+ */
 int _abs(int n)
 {
 	if (n >= 0)
