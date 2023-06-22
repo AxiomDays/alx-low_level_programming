17f75f796ef6963ff74a9ebddf0ec402fa4c56c1
Axiom Days <tobeoke@gmail.com>
2023-06-22 01:48:46 +0100

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,11 @@
  * print_last_digit - Entry point
  *
  * Description: 'the program's descriptor'
- * @n - int n
+ * @n: int n
  * Return: Always 0 (Success)
  */
 int print_last_digit(int n)
 {
-	
 	int m = n % 10;
 
 	if (m < 0)
