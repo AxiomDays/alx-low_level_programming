1f4afb139e4007af82d5fb282b3ea25d55763b6d
Axiom Days <tobeoke@gmail.com>
2023-06-23 12:22:27 +0100

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * mul - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @a: int a
+ * @b: int b
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return(m);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
 int _islower(int c);
