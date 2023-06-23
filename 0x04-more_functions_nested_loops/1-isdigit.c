5ba1d4c60bcab5f8ee259323517587505029eebf
Axiom Days <tobeoke@gmail.com>
2023-06-23 11:59:47 +0100

    task1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * _isdigit - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @c: int c
+ * Return: Always 0 (Success)
+ */
+int _isdigit(int c)
+{
+	if (c > 48 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+int _isdigit(int c);
 int _isupper(int c);
 int _islower(int c);
 int _putchar(char c);
