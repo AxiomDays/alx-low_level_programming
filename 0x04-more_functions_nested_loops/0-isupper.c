d5a4cf487913a7d24a6d097350090d9d95bc31b1
Axiom Days <tobeoke@gmail.com>
2023-06-23 11:40:23 +0100

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include<stdio.h>
 
+int _isupper(int c);
 int _islower(int c);
 int _putchar(char c);
