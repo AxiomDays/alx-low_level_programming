aeb6d0488610cb9a4b8d54d5753c34a3c0be4457
Axiom Days <tobeoke@gmail.com>
2023-06-23 20:14:26 +0100

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,24 +11,17 @@ void print_diagonal(int n)
 {
 	int i;
 	int j;
+	
+	if(n <= 0)
+		_putchar('\n');
 
 	for (i = 0; i < n; i++)
 	{
-		if (n <= 0)
+		for (j = 0; j < i; j++)
 		{
-			break;
+			_putchar(' ');
 		}
-		else
-		{
-			for (i = 0; i < n; i++)
-			{
-				for (j = 0; j < i; j++)
-				{
-					_putchar(' ');
-				}
-				_putchar('\\');
-				_putchar('\n');
-			}
-		}
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
