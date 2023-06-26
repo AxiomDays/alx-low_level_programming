fbb6283633f2b5fb4b129b07e9f905f7c68edeb3
Axiom Days <tobeoke@gmail.com>
2023-06-26 23:58:39 +0100

    task0

---
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include<stdio>
+#include"main.h"
+/**
+ * reset_to_98 - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @*n: int *n
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	p = *n;
+	p = 98;
+}
