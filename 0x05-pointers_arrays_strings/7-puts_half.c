ac8adc764f89d187fe4eb3f9a26f6dce61f8620c
Axiom Days <tobeoke@gmail.com>
2023-06-27 13:07:14 +0100

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ * puts_half - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @str: char str
+ * Return: Always 0 (Success)
+ */
+void puts_half(char *str)
+{
+	int i, j;
+	int len = 0;
+
+	for (i = 0; str[len] != '\0'; i++)
+	{
+		len++;
+	}
+	
+	j = len / 2;
+
+	for (len = j; str[len] != '\0'; len++)
+	{
+		_putchar(str[len]);
+		printf("second loop started");
+	}
+ 	_putchar('\n');
+}
