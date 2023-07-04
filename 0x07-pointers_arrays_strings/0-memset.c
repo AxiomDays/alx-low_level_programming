5c3116aa569c323fcc9d0a24fac54cd23f3dead9
Axiom Days <tobeoke@gmail.com>
2023-07-04 22:58:45 +0100

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 #include "main.h"
-
+/**
+ * *_memset - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @s: char s
+ * @b: char b
+ * @n: int n
+ * Return: Always 0 (Success)
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *p = s;
