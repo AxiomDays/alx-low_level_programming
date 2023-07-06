4f6e262bb91722ca22064bca325b211d2623aab0
Axiom Days <tobeoke@gmail.com>
2023-07-07 00:17:35 +0100

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * nrtcheck - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @a: int a
+ * @b: int b
+ * Return: Always 0 (Success)
+ */
+int nrtcheck(int a, int b)
+{
+	if (a * a == b)
+		return (a);
+	if (a * a > b)
+		return (-1);
+	return (nrtcheck(a + 1, b));
+}
+
+/**
+ * _sqrt_recursion - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @n: int n
+ * Return: Always 0 (Success)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (nrtcheck(1,n));
+}
