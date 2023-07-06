36ae2168ef82503571542281bdba3ab2d59476f4
Axiom Days <tobeoke@gmail.com>
2023-07-06 22:37:47 +0100

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @s: string
+ * Return: Always 0 (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursions(s + '1');
+		_putchar(*s);
+	}
+}
