189c53015e3fac94c445619ee83f6393d30af405
Axiom Days <tobeoke@gmail.com>
2023-07-08 01:30:43 +0100

    task0

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,2 +1,10 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @c: int c
+ * Return: Always 0 (Success)
+ */
 int _isupper(int c){
 }
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,5 @@
+#include "main.c"
+
+char *_strpbrk(char *s, char *accept)
+{
+}
