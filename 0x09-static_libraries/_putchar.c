a0c925d702dd13bd5fa067195e6db3014ee23cf6
Axiom Days <tobeoke@gmail.com>
2023-07-08 02:39:54 +0100

    task0?

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,11 +1,23 @@
-#include "main.h"
+#include <unistd.h>
+
+
+
 /**
- * _putchar - Entry point
  *
- * Description: 'the program's descriptor'
- * @c: char c
- * Return: Always 0 (Success)
- */
+ *  * _putchar - writes the character c to stdout
+ *
+ *   * @c: The character to print
+ *
+ *    *
+ *
+ *     * Return: On success 1.
+ *
+ *      * On error, -1 is returned, and errno is set appropriately*/
+
 int _putchar(char c)
+
 {
+
+		return (write(1, &c, 1));
+
 }
