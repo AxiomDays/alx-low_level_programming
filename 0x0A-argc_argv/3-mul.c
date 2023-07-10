d373b728d419f0d1cd18ef034f0f0767c27d9bac
Axiom Days <tobeoke@gmail.com>
2023-07-10 21:26:10 +0100

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @argc: int argc
+ * @argv: char argv
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+	int valuex;
+	int valuey;
+
+	if (argc != 3)
+	{
+		printf("Error");
+		putchar('\n');
+		return (1);
+	}
+	else
+	{
+		valuex = strtol(argv[1], NULL, 10);
+		valuey = strtol(argv[2], NULL, 10);
+		mul = valuex * valuey;
+		printf("%d", mul);
+		return (0);
+		putchar('\n');
+	}
+}
