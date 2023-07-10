7a09fc17b29e56f563f20274ca5092748077dc54
Axiom Days <tobeoke@gmail.com>
2023-07-10 22:40:58 +0100

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @argc: int argc
+ * @argv: char argv
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+	int add = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		sum = strtol(argv[i], NULL, 10);
+		if (argc < 2)
+		{
+			printf("0");
+			putchar('\n');
+			break;
+		}
+		else if (isdigit(sum))
+		{
+			printf("Error");
+			putchar('\n');
+			return (1);
+			break;
+		}
+		else
+		{
+			for (i = 1; i < argc; i++)
+			{
+				sum = strtol(argv[i], NULL, 10);
+				add = add + sum;
+			}
+			printf("%d", add);
+			putchar('\n');
+			break;
+		}
+	}
+		return (0);
+}
