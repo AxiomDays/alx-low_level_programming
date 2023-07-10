33d84bcd2af87d450a379cb1a535ac71d4fffaf6
Axiom Days <tobeoke@gmail.com>
2023-07-10 19:37:42 +0100

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @argc: int argc
+ * @argv: char argv
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char* argv[])
 {
 	printf("%s", argv[0]);
