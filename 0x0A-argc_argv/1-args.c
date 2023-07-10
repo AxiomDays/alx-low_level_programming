854486fca1145528e9094a3ced257cc78048cc25
Axiom Days <tobeoke@gmail.com>
2023-07-10 20:17:34 +0100

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @argc: int argc
+ * @argv: char argv
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char* argv[])
 {
 	printf("%d", (argc - 1));
+	(void)argv;
 	return (0);
 }
