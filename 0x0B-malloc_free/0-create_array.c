7081fcf5c033dceb201e52bce554c1dd0d3bfe28
Axiom Days <tobeoke@gmail.com>
2023-07-12 00:16:28 +0100

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
  * Return: Always 0 (Success)
  */
 char *create_array(unsigned int size, char c)
-{	
+{
 	char *i;
 	unsigned int k;
 
@@ -18,7 +18,6 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
 	i = (char *)malloc(size * sizeof(char));
 
 	if (i == NULL)
