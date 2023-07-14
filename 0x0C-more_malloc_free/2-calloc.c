c9e0895d2865cdd7433c018b313dd6a8554902f4
Axiom Days <tobeoke@gmail.com>
2023-07-14 04:43:08 +0100

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+#include<stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *x;
+	unsigned int y;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	x = malloc(size * nmemb);
+	if (x == NULL)
+		return (NULL);
+
+	for (y = 0; y < (nmemb * size); y++)
+	{
+		x[y] = 0;
+	}
+	return (x);
+}
