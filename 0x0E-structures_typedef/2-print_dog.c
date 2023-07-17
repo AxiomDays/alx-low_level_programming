e16d014b1835dd2f31527bf61d7408ca350ea469
Axiom Days <tobeoke@gmail.com>
2023-07-17 20:12:07 +0100

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,22 +2,28 @@
 #include "dog.h"
 
 void print_dog(struct dog *d)
-{
-	if (d->name != NULL)
+{	if (d == NULL)
 	{
-		printf("Name: %s\n", d->name);
+		printf(" ");
 	}
 	else
 	{
-		printf("Name: (nil)\n");
-	}
-	printf("Age: %.6f\n", d->age);
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
