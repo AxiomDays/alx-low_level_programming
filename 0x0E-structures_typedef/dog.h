cbdb776627a41ffc99d1a0b4badbca02ab8c9099
Axiom Days <tobeoke@gmail.com>
2023-07-17 18:41:53 +0100

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,12 @@
+/**
+ * struct dog - Entry point
+ * @name: char name
+ * @age: float age
+ * @owner: char owner
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
 struct dog 
 {
 	char *name;
