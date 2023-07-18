e5b156312d16ac14ade78d63f43c00dedf908282
Axiom Days <tobeoke@gmail.com>
2023-07-18 11:49:04 +0100

    cleanup4

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * init_dog - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @d: struct dog d
+ * @name: char *name
+ * @age: float age
+ * @owner: char *owner
+ * Return: Always 0 (Success)
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
