0880ccd61331c0f8c44e52970dbbc1d77feec901
Axiom Days <tobeoke@gmail.com>
2023-07-19 23:08:53 +0100

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * print_name - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @name: char name
+ * @f: void f
+ * Return: Always 0 (Success)
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
