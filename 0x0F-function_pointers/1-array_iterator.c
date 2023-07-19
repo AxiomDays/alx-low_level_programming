6eb74b4151cd7a338593091f09a06d2e6b08a89f
Axiom Days <tobeoke@gmail.com>
2023-07-19 23:39:28 +0100

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
+/**
+ * array_iterator - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @array: int array
+ * @size: size_t size
+ * @action: void action
+ * Return: Always 0 (Success)
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
