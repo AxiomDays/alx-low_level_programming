105b6273588cc77673fdeec62a7c29a031c343a4
Axiom Days <tobeoke@gmail.com>
2023-07-20 22:32:59 +0100

    task3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int (*get_op_func(char *s)) (int, int)
+{
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int op_add(int a, int b)
+{
+	int i;
+
+	i = a + b;
+	return (i);
+}
+int op_sub(int a, int b)
+{
+	int i;
+
+	i = a - b;
+	return (i);
+}
+int op_mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
+int op_div(int a, int b)
+{
+        int i;
+
+	i = a / b;
+	return (i);
+}
+int op_mod(int a, int b)
+{
+        int i;
+
+        i = a % b;
+        return (i);
+}
+
+
+
