27894669b781743cbafb7c3157edee2caf09d2ad
Axiom Days <tobeoke@gmail.com>
2023-07-28 02:17:13 +0100

    taskette

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_list - Entry point
+ *
+ * Description: 'the program's descriptor'
+ * @list_t n: const list_t *n
+ * Return: Always 0 (Success)
+ */
 size_t print_list(const list_t *h)
 {
 	int s = 0;
