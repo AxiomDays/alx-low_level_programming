fa3ab1be1725caff5af4c6dd4ffadce6775c8750
Axiom Days <tobeoke@gmail.com>
2023-08-02 00:29:08 +0100

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+void free_listint2(listint_t **head)
+{
+	listint_t *next;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+}
