50e45d3ee7ccf77302af10030a1ac8ef8934ca4a
Axiom Days <tobeoke@gmail.com>
2023-09-29 00:02:14 +0100

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,31 +17,25 @@ unsigned int binary_to_uint(const char *b)
 	if (b == 0)
 	{
 		return 0;
-		printf("string is null");
 	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
-			printf("non binary value found\n");
 			return 0;
 			break;
 		}
 		else
 		{
 			bin_num = bin_num * 10 + (b[i] - 48);
-			printf("bin_num = %d\n", bin_num);
 		}
 	}
 	while (bin_num != 0)
 	{
 		rem = bin_num % 10;
-		printf("%d ", dec_num);
 		bin_num = bin_num / 10;
 		dec_num = dec_num + rem * base;
 		base = base * 2;
-		printf("%d %d\n", bin_num, dec_num);
 	}
-	printf("test ");
 	return dec_num;
 }
