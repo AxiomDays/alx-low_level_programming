f9473e86e7328bf871e3f01bfc576a95c3c451af
Axiom Days <tobeoke@gmail.com>
2023-09-29 01:09:11 +0100

    task2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_binary(unsigned long int n)
+{
+	while (n != 0)
+	{
+		
+	}
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's descriptor'
+ *
+ * Return: Always 0 (Success)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i, ret;
+	unsigned long int bin;
+
+	bin  = 1 << index;
+	ret = n & bin
+
+	if (ret == bin)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
